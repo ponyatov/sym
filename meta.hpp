ebfd6c4af40ae8f2a3e9158317c281aafd6a7103
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-14 18:01:18 +0400

    * README/meta + docstring + $~ eval/quote + @ * TOC() tokenizer macro

--- meta.hpp
+++ meta.hpp
@@ -3,7 +3,7 @@
 #define MODULE	"sym"
 #define BRANCH	"master"
 #define ABOUT	"Reference SYMbolic Engine (Dynamic Language Runtime)"
-#define AUTHOR	"Dmitry Ponyatov <dponyatov@gmail.com>"
+#define AUTHOR	"Dmitry Ponyatov <<dponyatov@gmail.com>>"
 #define LICENSE	"GNU LesserGPL"
 #define GITHUB	("https://github.com/ponyatov/" MODULE "/tree/" BRANCH)
 #endif // _H_META
