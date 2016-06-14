87d53582c4b75f558738ce9e037d903952891040
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-14 10:22:12 +0400

    * meta
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta.hpp
+++ meta.hpp
@@ -1,6 +1,9 @@
+#ifndef _H_META
+#define _H_META
 #define MODULE	"sym"
-#define ABOUT	"Reference SYMbolic Engine (Dynamic Language Runtime)"
+#define BRANCH	"mini"
+#define ABOUT	"minimal lexical program skeleton & DLR"
 #define AUTHOR	"Dmitry Ponyatov <dponyatov@gmail.com>"
 #define LICENSE	"GNU LesserGPL"
-#define GITHUB	("https://github.com/ponyatov/" MODULE "/")
-#define LOGO	"logo64x64"
+#define GITHUB	("https://github.com/ponyatov/" MODULE "/tree/" BRANCH)
+#endif // _H_META
